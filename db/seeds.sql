46e4ae07fbbcf61b8edf2d1bfe73c7056111875c
gabriellatrujillor <gabriella.trujillor@gmail.com>
2020-06-28 17:14:14 -0600

    edited burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name) VALUES ('Elephant Burger');
-INSERT INTO burgers (burger_name) VALUES ('Cliff House Burger');
-INSERT INTO burgers (burger_name) VALUES ('Cheesehead Burger');
-INSERT INTO burgers (burger_name) VALUES ('The Clumsy Burger');
-INSERT INTO burgers (burger_name) VALUES ('The Clara Burger');
+INSERT INTO burgers (burger_name) VALUES ('Yer a Burger, Harry');
+INSERT INTO burgers (burger_name) VALUES ('Potter and the Burger of Fire');
+INSERT INTO burgers (burger_name) VALUES ('The Halfblood Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fleur Burgercore');
+INSERT INTO burgers (burger_name) VALUES ('Constant Vigilance Burger');
