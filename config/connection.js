54f465314f94c20fef24e8360af516f04147eef0
gabriellatrujillor <gabriella.trujillor@gmail.com>
2020-06-30 13:35:35 -0600

    syncing up jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // setup mysql connection
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+let connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection= mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Inuyasha!34!",
-  database: "burger_db",
-});
+  database: "burger_db",  
+  });
+
+}
+
 
 // make connection
 connection.connect((err) => {
