bd4319f19165370da00813ae76dd17f117ef18f7
gabriellatrujillor <gabriella.trujillor@gmail.com>
2020-06-30 11:37:35 -0600

    testing names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ('Yer a Burger, Harry');
-INSERT INTO burgers (burger_name) VALUES ('Potter and the Burger of Fire');
+INSERT INTO burgers (burger_name) VALUES ('Harry Potter and the Burger of Fire');
 INSERT INTO burgers (burger_name) VALUES ('The Halfblood Burger');
-INSERT INTO burgers (burger_name) VALUES ('Fleur Burgercore');
-INSERT INTO burgers (burger_name) VALUES ('Constant Vigilance Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Malfoy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whizzard Weezies Burger');
